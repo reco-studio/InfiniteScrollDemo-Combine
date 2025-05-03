cb8281985222fb6e81efbadde81a7ca8bea69214
Maciej Szostak <info@reco.studio>
2025-05-03 12:10:48 +0200

    Initial Commit

---
+++ InfiniteScrollDemo/InfiniteScrollDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollDemoApp.swift
+//  InfiniteScrollDemo
+//
+//  Created by Maciej Szostak on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
